a675ad3327670da62a8f744e5b679891bfcd6155
Deepvodolaz <110783453+Deepvodolaz@users.noreply.github.com>
2022-08-14 18:11:29 +0300

    Создание файла CsvAnalystDZ.py

---
+++ CsvAnalystDZ.py
@@ -0,0 +1,12 @@
+
+
+import tkinter as tk
+
+# Создание главного окна
+window=tk.Tk()
+window.geometry("550x550")
+window.title("Программа обработки .csv файлов")
+
+
+# Запуск цикла mainloop
+window.mainloop()
